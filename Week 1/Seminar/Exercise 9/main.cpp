977bd931c3e0a85d0f870b65c1dbfbb5954644d9
Nadya Radeva <116949953+NadyaRadeva@users.noreply.github.com>
2024-10-09 09:09:13 +0300

    Update main.cpp

--- Week 1/Seminar/Exercise 9/main.cpp
+++ Week 1/Seminar/Exercise 9/main.cpp
@@ -4,7 +4,7 @@ int main() {
   char ch;
   std::cout << "Enter a character: ";
   std::cin >> ch;
-  int charToInt = (int)ch;
+  int charToInt = ch - '0';
   std::cout << "Char to int: " << charToInt << std::endl;
 
   return 1;
